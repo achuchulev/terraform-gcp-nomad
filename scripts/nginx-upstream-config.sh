6ee50c687d783081c598af647f0e784cd3defcb5
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-09-02 12:57:20 +0300

    Update nginx-upstream-config.sh

--- scripts/nginx-upstream-config.sh
+++ scripts/nginx-upstream-config.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-[ -f /etc/nginx/sites-available/default ] && { BACKEND_SOCKET=`sudo cat /etc/nginx/sites-available/default | grep 'upstream nomad_backend'`
-    if [ '${BACKEND_SOCKET}' == '' ]; 
-        then 
-            sudo -E bash -c 'echo upstream nomad_backend { $OUT } >> /etc/nginx/sites-available/default' 
-        else 
-            sed -i 's/${BACKEND_SOCKET}/upstream nomad_backend { $OUT }/g' /etc/nginx/sites-available/default
-    fi 
+[ -f /etc/nginx/sites-available/default ] && { BACKEND_SOCKET=`cat /etc/nginx/sites-available/default | grep 'upstream nomad_backend'`
+    if [ "${BACKEND_SOCKET}" == "" ];
+        then
+            echo "upstream nomad_backend { $1 }" >> /etc/nginx/sites-available/default
+        else
+            sed -i "s/${BACKEND_SOCKET}/upstream nomad_backend { $1 }/g" /etc/nginx/sites-available/default
+    fi
 }
